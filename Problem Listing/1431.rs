31bd8c42d753bb1f451a8801337d3ddd12faba38
Bruno Firmino <brunoftlk@gmail.com>
2022-01-11 18:41:42 +0000

    Create 1431.rs

---
+++ Problem Listing/1431.rs
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+impl Solution {
+    pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
+        let len = candies.len();
+        let greatest = *candies.iter().max().unwrap();
+        let mut res: Vec<bool> = Vec::with_capacity(len);
+        for x in 0..len {
+            let total = candies[x]+extra_candies;
+            if total >= greatest {
+                res.push(true);
+            } else {
+                res.push(false);
+            }
+        }
+        return res;
+    }
+}
