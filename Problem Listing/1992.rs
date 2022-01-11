d5fb9b2193b03d5646040472d6a1e816fcbfffc9
Bruno Firmino <brunoftlk@gmail.com>
2022-01-11 14:45:08 +0000

    Create 1992.rs

---
+++ Problem Listing/1992.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+        let len = nums.len();
+        let mut res: Vec<i32> = Vec::with_capacity(len*2);
+        res.extend(&nums);
+        res.extend(&nums);
+        return res;
+    }
+}
