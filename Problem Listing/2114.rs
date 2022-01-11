bf66c363619a58d79b02488035e8a584b3931608
Bruno Firmino <brunoftlk@gmail.com>
2022-01-11 14:59:51 +0000

    Create 2114.rs

---
+++ Problem Listing/2114.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn most_words_found(sentences: Vec<String>) -> i32 {
+        let mut res = 0;
+        let it = sentences.iter();
+        for x in it {
+            let count = x.matches(' ').count()+1;
+            if count > res {
+                res = count;
+            }
+        }
+        return res as i32;
+    }
+}
