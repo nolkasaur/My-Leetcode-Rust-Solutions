11bc708317f0029739fd3ffc06980dbedbef2a18
Bruno Firmino <brunoftlk@gmail.com>
2022-01-11 15:04:11 +0000

    Update 2114.rs

--- Problem Listing/2114.rs
+++ Problem Listing/2114.rs
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+
 impl Solution {
     pub fn most_words_found(sentences: Vec<String>) -> i32 {
         let mut res = 0;
