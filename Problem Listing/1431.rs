8e4a61d2cdd63c81d65207b1ff6999e54efca7c6
Bruno Firmino <brunoftlk@gmail.com>
2022-01-11 21:10:07 +0000

    Update 1431.rs

--- Problem Listing/1431.rs
+++ Problem Listing/1431.rs
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+// 0 ms, 2.1 MB
 
 impl Solution {
     pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
