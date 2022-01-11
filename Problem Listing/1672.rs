df7e1295341bb80c66194b3a682ef7b1c5408331
Bruno Firmino <brunoftlk@gmail.com>
2022-01-11 18:11:59 +0000

    Create 1672.rs

---
+++ Problem Listing/1672.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/richest-customer-wealth/
+
+impl Solution {
+    pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
+        let mut maxWealth: i32 = 0;
+        let it = accounts.iter();
+        for val in it {
+            let sum: i32 = val.iter().sum();
+            if sum > maxWealth {
+                maxWealth = sum;
+            }
+        }
+        return maxWealth;
+    }
+}
