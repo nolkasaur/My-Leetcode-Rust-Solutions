ff150ed01a0ce98e17b032fd04adb7080bca8f10
Bruno Firmino <brunoftlk@gmail.com>
2022-01-11 18:51:43 +0000

    Create 1512.rs

---
+++ Problem Listing/1512.rs
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/number-of-good-pairs/
+
+impl Solution {
+    pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
+        let len = nums.len();
+        let mut count = 0;
+        for x in 0..len {
+            for y in x+1..len {
+                if nums[x] == nums[y] {
+                    count +=1;
+                }
+            }
+        }
+        return count;
+    }
+}
