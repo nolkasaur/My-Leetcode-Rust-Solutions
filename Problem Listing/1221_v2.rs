1c7119790ef178cfc722c560589a4279d7d57bbf
Bruno Firmino <brunoftlk@gmail.com>
2022-01-12 19:06:37 +0000

    Create 1221_v2.rs

---
+++ Problem Listing/1221_v2.rs
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/split-a-string-in-balanced-strings/
+// 0 ms, 2 MB
+
+impl Solution {
+    pub fn balanced_string_split(s: String) -> i32 {
+        let mut res = 0;
+        let mut lBal = 0;
+        for x in 0..s.len() {
+            if s.chars().nth(x).unwrap() == 'L' {
+                lBal+=1;
+            } else {
+                lBal-=1;
+            }
+            if lBal == 0 { res+=1; }
+        }
+        return res;
+    }
+}
