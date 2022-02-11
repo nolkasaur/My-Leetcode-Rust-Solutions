40d29c92102f66f7233072502b1757f641821f72
Bruno Firmino <brunoftlk@gmail.com>
2022-02-11 12:06:28 +0000

    Create 0912.rs

---
+++ Problem Listing/0912.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sort-an-array/
+// 26 ms, 2.8 MB
+
+impl Solution {
+    pub fn sort_array(mut nums: Vec<i32>) -> Vec<i32> {
+        nums.sort();
+        return nums;
+    }
+}
